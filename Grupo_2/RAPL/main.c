b5eaeb44d90236be057c1f755b6dfae7d2fa4218
DaniloQOliveira <dqoliveira21@gmail.com>
2023-03-28 02:39:02 +0100

    formated file

--- Grupo_2/RAPL/main.c
+++ Grupo_2/RAPL/main.c
@@ -1,10 +1,9 @@
 
 /*
  *  Análise e Teste de Software
- *  João Saraiva 
+ *  João Saraiva
  *  2016-2017
  */
-
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -14,85 +13,86 @@
 
 #define RUNTIME
 
-
-int main (int argc, char **argv) 
-{ char command[500],res[500];
-  int  ntimes = 1;
-  int  core = 0;
-  int  i=0;
+int main(int argc, char **argv)
+{
+  char command[500], res[500];
+  int ntimes = 1;
+  int core = 0;
+  int i = 0;
 
 #ifdef RUNTIME
   clock_t begin, end;
   double time_spent;
 
-  struct timeval tvb,tva;
-  
+  struct timeval tvb, tva;
+
 #endif
-  
-  FILE * fp;
+
+  FILE *fp;
 
   // printf("Program to be executed: %d",argc);
-  //strcpy( command, "./" );
- strcat(command,argv[1]);
- printf("Program to be executed: %s\n",argv[1]);
- if(strstr(argv[1],"py")) {
-	strcpy(command, " " ); 
- }
- else {
- 	strcpy(command, "./" );
- }
-  
- 
-  strcat(command,argv[1]);
+  // strcpy( command, "./" );
+  strcat(command, argv[1]);
+  printf("Program to be executed: %s\n", argv[1]);
+  if (strstr(argv[1], "py"))
+  {
+    strcpy(command, " ");
+  }
+  else
+  {
+    strcpy(command, "./");
+  }
 
-  ntimes = atoi (argv[2]);
+  strcat(command, argv[1]);
 
+  ntimes = atoi(argv[2]);
 
-  strcpy(res,command);
-  for (i = 0; res[i] != '\0'; i++) {
-        if (res[i] == '/') {
-            res[i] = '_';
-        }
+  strcpy(res, command);
+  for (i = 0; res[i] != '\0'; i++)
+  {
+    if (res[i] == '/')
+    {
+      res[i] = '_';
     }
-  strcat(res,".J");
-  printf("Command: %s  %d-times res: %s\n",command,ntimes,res);
-  
+  }
+  strcat(res, ".J");
+  printf("Command: %s  %d-times res: %s\n", command, ntimes, res);
 
-  printf("\n\n RUNNING THE PARAMETRIZED PROGRAM:  %s\n\n\n",command);
+  printf("\n\n RUNNING THE PARAMETRIZED PROGRAM:  %s\n\n\n", command);
   fflush(stdout);
-  
-  fp = fopen(res,"w");
+
+  fp = fopen(res, "w");
   rapl_init(core);
 
-  fprintf(fp,"Program, Package , Core(s) , GPU , DRAM? , Time (sec) \n");
+  fprintf(fp, "Program, Package , Core(s) , GPU , DRAM? , Time (sec) \n");
 
-  
-  for (i = 0 ; i < ntimes ; i++)
-    {   sleep(1);                                    // sleep 1 second
-        fprintf(fp,"%s , ",argv[1]);
-        rapl_before(fp,core);
-      
-#ifdef RUNTIME
-        begin = clock();
-	gettimeofday(&tvb, 0);
-#endif
-	
-        system(command);
+  for (i = 0; i < ntimes; i++)
+  {
+    sleep(1); // sleep 1 second
+    fprintf(fp, "%s , ", argv[1]);
+    rapl_before(fp, core);
 
 #ifdef RUNTIME
-	end = clock();
-	gettimeofday(&tva, 0);
-	//	time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
-	time_spent = (tva.tv_sec-tvb.tv_sec)*1000000 + tva.tv_usec-tvb.tv_usec;
+    begin = clock();
+    gettimeofday(&tvb, 0);
 #endif
 
-	rapl_after(fp,core);
+    system(command);
 
-#ifdef RUNTIME	
-	fprintf(fp," %G \n",time_spent);
-#endif	
-    }
-    
+#ifdef RUNTIME
+    end = clock();
+    gettimeofday(&tva, 0);
+    //	time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+    time_spent = (tva.tv_sec - tvb.tv_sec) * 1000000 + tva.tv_usec - tvb.tv_usec;
+#endif
+
+    rapl_after(fp, core);
+
+#ifdef RUNTIME
+    fprintf(fp, " %G \n", time_spent);
+#endif
+  }
+
   printf("\n\n END of PARAMETRIZED PROGRAM: \n");
 
   fclose(fp);
@@ -100,6 +100,3 @@ int main (int argc, char **argv)
 
   return 0;
 }
-
-
-
